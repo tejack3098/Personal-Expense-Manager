f7c72bedccdeaaa11b8d0b7f8e33f2f220582fd7
Tejack98 <choudhari.t@northeastern.edu>
2024-11-24 14:39:35 -0500

    Initial Commit

---
+++ expense-tracker/src/test/java/com/tejack/expense/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejack.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
