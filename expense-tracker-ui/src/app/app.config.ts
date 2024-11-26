c8858a3391cd68c92e18262b05d1de841d80daf7
Tejack98 <choudhari.t@northeastern.edu>
2024-11-26 09:59:20 -0500

    Updated app config

--- expense-tracker-ui/src/app/app.config.ts
+++ expense-tracker-ui/src/app/app.config.ts
@@ -1,9 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HTTP_INTERCEPTORS} from '@angular/common/http';
+import { MyInterceptorService } from './services/my-interceptor.service';
+import { provideHttpClient } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  
+  providers: [
+    provideHttpClient(),
+    { provide: HTTP_INTERCEPTORS, useClass: MyInterceptorService, multi: true },
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+  ]
 };
